434c00f7f6505d65b5d6a40ea4e1704111085ac2
Jorge Renato Macedo <renato.2208@gmail.com>
2026-02-20 17:27:42 -0600

    classes

--- capstone/urls.py
+++ capstone/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('crm.urls'))
-
+    path('', include('crm.urls')),
+    path('notifications/', include('notifications.urls')),
 
     # API Routes
 ]
