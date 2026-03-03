e86b39846e6ab2281085c48b31eba7b47296fdd8
Jorge Renato Macedo <renato.2208@.com>
2026-03-03 23:23:32 +0000

    WIP: update urls, login layout, utils; add logo.svg and README

--- capstone/urls.py
+++ capstone/urls.py
@@ -22,8 +22,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('crm.urls')),
-    path('notifications/', include('notifications.urls')),
-    #path('api/', include('api.urls')),
+    #path('notifications/', include('notifications.urls')),
+
     # API Routes
 ]
 
